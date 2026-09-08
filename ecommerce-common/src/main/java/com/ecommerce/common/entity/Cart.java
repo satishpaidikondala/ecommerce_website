688a4c66729cde309039fd43cf95ba2d9fd206a9
Satish Paidikondala <satishpaidikondala@example.com>
2026-09-08 12:06:51 +0530

    fix: Cart implements Serializable for Redis cache (DefaultSerializer)

--- ecommerce-common/src/main/java/com/ecommerce/common/entity/Cart.java
+++ ecommerce-common/src/main/java/com/ecommerce/common/entity/Cart.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -17,7 +18,8 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Cart {
+public class Cart implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
