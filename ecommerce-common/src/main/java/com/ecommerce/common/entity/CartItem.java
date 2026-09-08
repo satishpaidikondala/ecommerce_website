459efdb68b62dd7c934d755df9961ce409124bad
Satish Paidikondala <satishpaidikondala@example.com>
2026-09-08 12:07:07 +0530

    fix: CartItem implements Serializable for Redis

--- ecommerce-common/src/main/java/com/ecommerce/common/entity/CartItem.java
+++ ecommerce-common/src/main/java/com/ecommerce/common/entity/CartItem.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Entity
@@ -14,7 +15,8 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class CartItem {
+public class CartItem implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
